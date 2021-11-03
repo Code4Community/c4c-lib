1746a8930a3a93835e0f2f6bd878c1db62248c5b
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-11-03 19:35:44 -0400

    Partially configured editor for language (indentation not finished)

--- src/editor.js
+++ src/editor.js
@@ -1,20 +1,64 @@
-import {parser} from "./lang.grammar"
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
+import {
+  LRLanguage,
+  LanguageSupport,
+  foldNodeProp,
+  foldInside,
+  indentNodeProp,
+  continuedIndent,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@codemirror/highlight";
+import { completeFromList } from "@codemirror/autocomplete";
+import { parser as ourParser } from "./lang.js";
 
-var theme = EditorView.theme({
+const ourParserWithMetadata = ourParser.configure({
+  props: [
+    styleTags({
+      Statement: t.variableName,
+      Condition: t.bool,
+      Number: t.number,
+      Comment: t.comment,
+    }),
+    indentNodeProp.add({
+      IfExpression: continuedIndent({ except: /^(elif|else|end)\b/ }),
+      TimesExpression: continuedIndent({ except: /^end\b/ }),
+    }),
+    foldNodeProp.add({
+      IfExpression: foldInside,
+      TimesExpression: foldInside,
+    }),
+  ],
+});
+
+// Codemirror Support for Our Language
+const ourLanguage = LRLanguage.define({
+  parser: ourParserWithMetadata,
+  languageData: {
+    commentTokens: { line: "//" },
+    autocomplete: completeFromList([
+      { label: "times", type: "keyword" },
+      { label: "end", type: "keyword" },
+      { label: "move", type: "function" },
+      { label: "jump", type: "function" },
+    ]),
+  },
+});
+
+const ourLanguageSupport = new LanguageSupport(ourLanguage);
+
+// Creating the Editor View and Initial State
+const theme = EditorView.theme({
   ".cm-content": {
-      fontSize: "1.5em",
-      fontWeight: "500",
-      fontFamily: "'Oswald'",
-      color: "black"
-  }
-})
+    fontSize: "1.5em",
+    fontWeight: "500",
+    fontFamily: "'Oswald'",
+    color: "black",
+  },
+});
 
-let editor = new EditorView({
+const editor = new EditorView({
   state: EditorState.create({
-    extensions: [basicSetup, theme]
+    extensions: [ourLanguageSupport, basicSetup, theme],
   }),
-  parent: document.getElementById("editor")
-})
-
-console.log(parser)
+  parent: document.getElementById("editor"),
+});
